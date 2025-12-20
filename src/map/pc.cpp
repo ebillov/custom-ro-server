22015c9a9daa7a583044cd6393b86e658eb378e7
Virson Ebillo <virson.ebillo@c9.com.au>
2025-12-20 22:23:11 +0800

    Fixed random warp invalid values.

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -16189,7 +16189,7 @@ int32 pc_autoattack_timer(int32 tid, int64 tick, int32 id, intptr_t data)
 	//Get mapindex and verify if the autoattack instance mapindex is the same as the origin mapindex
 	if (sd->autoattack_origin_mapindex != sd->mapindex ) {
 		//Different map, get back to map origin
-		pc_setpos( sd, sd->autoattack_origin_mapindex, -1, -1, CLR_TELEPORT );
+		pc_setpos( sd, sd->autoattack_origin_mapindex, 0, 0, CLR_TELEPORT );
 	}
 
 	//Set character effect to stoned state (only animation is applied)
